a96fcff1c0189d3ce1ef2ab095aea83f8aa54e77
AlexandraDaza <agallod@unal.edu.co>
2022-04-17 21:37:52 -0500

    mas funcionalidades

--- app.js
+++ app.js
@@ -5,32 +5,70 @@ const concesionaria = {
   
   
     buscarAuto: function(patente) {
-      for (let i = 0; i < autos.length; i++){
-        if (autos[i].patente == patente) {
-          return autos[i]
-        }else
-        return null;
-    }
-},
-    venderAuto: function (patente) {
-    const auto = this.buscarAuto(patente);
-    if (auto) {
-      auto.vendido = true;
-    }
-},
-     autosParaLaVenta: function autosParaLaVenta(){ 
-       let autosALaVenta = this.autos.filter(function(objeto){
-     return objeto.vendido ==
-     false
-   })
-  return autosALaVenta
-     }
-    },
-    autosNuevos:function autoNuevo=> {
-      let autosALaVenta = this.autosParaLaVenta()
-      let autosNuevos = autosALaVenta.filter(function(objeto){
-          return objeto.km < 100
+      let auto = this.autos.filter(function(objeto){
+        return objeto.patente == patente
       })
-      return autosNuevos;
+      if(auto != null){
+        return auto
+      }else{
+        return null
+      }
+    },
+    venderAuto: function (patenteBuscar) {
+        let patenteBuscada= this.buscarAuto(patenteBuscar).patente;
+        if ((patenteBuscada !=null)){
+          return venderAuto
     }
   },
+    autosParaLaVenta: function autosParaLaVenta(){
+     let autosALaVenta = this.autos.filter(function(objeto){
+      return objeto.vendido == false
+      })
+      return autosALaVenta
+    },
+    autosNuevos: function autosNuevos(){
+      let autosALaVenta = this.autosParaLaVenta().filter(function(objeto){ 
+       return objeto.km < 100  
+      })    
+      return autosALaVenta
+    },
+    
+    listaDeVentas: function (patente){
+      let lista = [];
+      const ventas= autos.filter(function(objeto){
+        if ( objeto.vendido==true){
+          return lista.push(objeto.precio);
+        }
+      })
+      let map = ventas.map(function(elemento){
+        if ( elemento.patente == patente){
+          return lista
+        }
+      })
+     return map
+    },
+       totalDeVentas: function totalDeVentas(){
+    let ventas = this.listaDeVentas()
+    if(ventas[0] == null){
+        return 0
+    }
+    else{
+    let total = ventas.reduce(function(estado, numero){
+        return estado + numero
+    })
+    return total
+    }
+}
+}
+console.log(concesionaria.totalDeVentas())
+  /*
+  for (i = 0; i < ventas.length; i++){
+  
+  autosNuevos: function autosOKM(){
+    let auto = this.autosParaLaVenta(),
+    (function (objeto){
+      objeto.km < 100
+    return autosNuevos
+  console.log (concesionaria.autosNuevos('JJK116')
+{
+  */
